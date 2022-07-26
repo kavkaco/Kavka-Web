d72aaa2b095896e4707b1468279b267bc3321020
Taha Dostifam <mr.tahadostifam@gmail.com>
2022-07-26 22:17:27 +0430

    init inputs & buttons components. sigin with email page finished

--- src/views/signin.vue
+++ src/views/signin.vue
@@ -1,7 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { MailIcon } from "@heroicons/vue/outline";
+</script>
 
 <template>
   <div>
-    <div class="form">aa</div>
+    <div class="form">
+      <div class="flex justify-between mb-1">
+        <h1 class="font-bold text-2xl">Signin</h1>
+        <button class="text-slate-600">Try another way</button>
+      </div>
+
+      <div class="input-row">
+        <label class="input-title">Email</label>
+        <input type="text" class="block w-full text-sm rounded-xl" />
+        <div class="input-icon">
+          <MailIcon />
+        </div>
+      </div>
+
+      <button type="button" class="btn btn-primary btn-animated-caret w-full rounded-xl p-2.5 mt-8">
+        Submit
+        <span class="next-caret">-></span>
+      </button>
+    </div>
   </div>
 </template>
