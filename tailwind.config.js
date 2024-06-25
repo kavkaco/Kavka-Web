7d3aacdd3676f1b4ec18798edbb698ad1816cefe
Taha Dostifam <mr.tahadostifam@gmail.com>
2024-06-25 23:57:42 +0330

    login page completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 /** @type {import('rippleui').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Poppins", "Vazirmatn"]
-      }
+        sans: ["Poppins", "Vazirmatn"],
+      },
     },
   },
   plugins: [require("rippleui")],
   rippleui: {
     themes: [
       {
-        themeName: "light",
-        colorScheme: "light",
-        colors: {
-          primary: "#4c24ff",
-          backgroundPrimary: "#964643",
-        },
-      },
-      {
         themeName: "dark",
         colorScheme: "dark",
         colors: {
-          primary: "#4c24ff",
-          backgroundPrimary: "#0d0d0d",
+          primary: "#1759f3",
+          backgroundPrimary: "#020202",
+          backgroundSecondary: "#141414"
         },
       },
     ],
   },
-}
-
+};
