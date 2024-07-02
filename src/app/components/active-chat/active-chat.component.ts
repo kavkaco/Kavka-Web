e68026deb95e4c14fd1fbaa50f035967829ef475
Taha Dostifam <mr.tahadostifam@gmail.com>
2024-07-02 21:37:30 +0330

    add smart voice button to active-chat->input-section

--- src/app/components/active-chat/active-chat.component.ts
+++ src/app/components/active-chat/active-chat.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { NgScrollbarModule } from 'ngx-scrollbar';
 import { AutoGrowingInputDirective } from '../../directives/auto-growing-input.directive';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-active-chat',
   standalone: true,
-  imports: [NgScrollbarModule, AutoGrowingInputDirective],
+  imports: [NgScrollbarModule, AutoGrowingInputDirective, FormsModule],
   templateUrl: './active-chat.component.html',
   styleUrl: './active-chat.component.scss',
 })
-export class ActiveChatComponent {}
+export class ActiveChatComponent {
+  textInput: string = '';
+}
