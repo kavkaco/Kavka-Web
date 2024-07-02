0cd3c88c1361e55bb4f939f9b13fdcd7fa34b3f5
Taha Dostifam <mr.tahadostifam@gmail.com>
2024-07-02 19:29:58 +0330

    design active chat done and added a great auto-grown-input written in angular-directives

--- src/app/components/active-chat/active-chat.component.ts
+++ src/app/components/active-chat/active-chat.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { NgScrollbarModule } from 'ngx-scrollbar';
+import { AutoGrowingInputDirective } from '../../directives/auto-growing-input.directive';
 
 @Component({
   selector: 'app-active-chat',
   standalone: true,
-  imports: [],
+  imports: [NgScrollbarModule, AutoGrowingInputDirective],
   templateUrl: './active-chat.component.html',
-  styleUrl: './active-chat.component.scss'
+  styleUrl: './active-chat.component.scss',
 })
-export class ActiveChatComponent {
-
-}
+export class ActiveChatComponent {}
---
+++ src/app/directives/auto-growing-input.directive.ts
@@ -0,0 +1,55 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appAutoGrowingInput]',
+  standalone: true,
+})
+export class AutoGrowingInputDirective {
+  @Input() initialHeight!: number;
+  @Input() lineHeight!: number;
+  @Input() borderWidth!: number;
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener('keyup') onKeyup() {
+    const textarea = this.el.nativeElement;
+
+    this.setOverflow('hidden');
+
+    if (textarea.value.trim() == '') {
+      textarea.style.height = this.initialHeight + 'px';
+      return;
+    }
+
+    const heightNumber = this.calculateInputHeight(textarea.value);
+    const heightCss = heightNumber + 'px';
+
+    if (heightNumber > 150) {
+      this.setOverflow('auto');
+      return;
+    }
+
+    textarea.style.height = heightCss;
+  }
+
+  calculateInputHeight(value: string) {
+    const numberOfLineBreaks = (value.match(/\n/g) || []).length;
+
+    console.log('numberOfLineBreaks', numberOfLineBreaks);
+
+    if (numberOfLineBreaks == 0) {
+      return this.initialHeight;
+    }
+
+    // min-height + lines x line-height + border
+    return (
+      this.initialHeight +
+      numberOfLineBreaks * this.lineHeight +
+      this.borderWidth
+    );
+  }
+
+  setOverflow(value: string) {
+    this.el.nativeElement.style.overflow = value;
+  }
+}
