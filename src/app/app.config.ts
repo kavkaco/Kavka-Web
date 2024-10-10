0e2f49dca01959879be1f8e9c26db48e9671d805
[ Taha. Dostifam ] <tahadostifam@localhost.localdomain>
2024-10-11 02:41:32 +0330

    push normal angular app version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
-import { provideRouter } from "@angular/router";
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-import { routes } from "./app.routes";
-import { provideClientHydration } from "@angular/platform-browser";
-import { provideServiceWorker } from "@angular/service-worker";
-import { provideState, provideStore } from "@ngrx/store";
+import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
+import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from "@ngrx/store-devtools";
-import { authReducer } from "./store/auth/auth.reducer";
-import { chatReducer } from "@app/store/chat/chat.reducers";
-import { messageReducer } from "@app/store/messages/messages.reducers";
-import { connectivityReducer } from "@app/store/connectivity/connectivity.reducers";
 import { provideAnimations } from "@angular/platform-browser/animations";
+import { connectivityReducer } from './store/connectivity/connectivity.reducers';
+import { authReducer } from './store/auth/auth.reducer';
+import { chatReducer } from './store/chat/chat.reducers';
+import { messageReducer } from './store/messages/messages.reducers';
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(routes),
-        provideClientHydration(),
-        provideServiceWorker("ngsw-worker.js", {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
+          provideServiceWorker("ngsw-worker.js", {
             enabled: true,
             registrationStrategy: "registerWhenStable:30000",
         }),
@@ -28,5 +27,5 @@ export const appConfig: ApplicationConfig = {
         provideState("auth", authReducer),
         provideState("chat", chatReducer),
         provideState("message", messageReducer),
-    ],
+      ]
 };
